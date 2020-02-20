c7af26cb176571fe2d9c1c6a7d57ad2943fa99da
Brett Packard <brettjayp@gmail.com>
2020-02-19 21:13:52 -0800

    it's alive

--- server.js
+++ server.js
@@ -1,70 +1,19 @@
-'use strict';
+// 'use strict'
 
-require('dotenv').config();
 const express = require('express');
-const pg = require('pg');
-const superagent = require('superagent');
-const methodOverride = require('method-override');
-
 const app = express();
-const client = new pg.Client(process.env.DATABASE_URL);
+require('dotenv');
 
-app.set('view engine', 'ejs');
+app.get('*', (req, res) => { res.sendFile('index.html', { root: './public' }) })
+// does nothing without app.listen
+// app.listen(3000)
+//   .then(console.log('alive on 3000'))
+//   .catch(console.log('uh og'))
+function startServer(){
+  const port = process.env.PORT || 3000;
+  app.listen(port)
+    // .then(() => console.log(`Server Listening on ${port}`))
+    // .catch(err => console.error(err));
+};
 
-app.use(express.urlencoded({ extened: true }));
-app.use(methodOverride('_method'));
-
-app.get('/', (req, res) => {
-  let SQL = "SELECT * FROM tasks";
-  client.query(SQL)
-    .then(results => {
-      res.render('pages/index.ejs', { tasks: results.rows })
-    })
-});
-
-app.get('/task/:id', (req, res) => {
-  let id = req.params.id;
-  let SQL = "SELECT * FROM tasks WHERE id = $1";
-  let values = [id];
-  client.query(SQL, values)
-    .then(results => {
-      res.render('pages/detail', { task: results.rows[0] })
-    })
-});
-
-app.post('/tasks', (req, res) => {
-  res.send('Created Task');
-});
-
-app.put('/task/:id', (req, res) => {
-  let id = req.params.id;
-  let task = req.body.task;
-  let assigned = req.body.assigned;
-  let status = req.body.status;
-
-  let SQL = "UPDATE tasks SET task=$1, assigned=$2, status=$3 WHERE id = $4";
-  let values = [task, assigned, status, id];
-
-  client.query(SQL, values)
-    .then(() => {
-      res.redirect(`/task/${id}`);
-    })
-    .catch(e => { throw e; })
-
-});
-
-app.delete('/task/:id', (req, res) => {
-  let id = req.params.id;
-  let SQL = "DELETE FROM tasks WHERE id = $1";
-  let values = [id];
-  client.query(SQL, values)
-    .then(() => {
-      res.redirect('/');
-    })
-    .catch(e => { throw e })
-});
-
-client.connect()
-  .then(() => {
-    app.listen(process.env.PORT, () => console.log(process.env.PORT))
-  });
+startServer();
