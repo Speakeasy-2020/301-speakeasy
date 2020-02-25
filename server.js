a3b702f0fda0a9d110606424f5feda84bef5386f
dirrbick <kory.a.jaxn@gmail.com>
2020-02-24 17:16:40 -0800

    committing to pull master

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.set('view engine', 'ejs');
 app.get('/', homePage);
 app.get('/event', eventRender);
 app.get('/guestList', guestListRender);
-app.get('/menuRender', menuPage);
+app.get('/menuRender', savedDatabase);
 app.get('/publicView', publicPage);
-
+// app.get('/savedDrink', savedDatabase);
 
 app.post('/event', storeUser);
 
@@ -33,7 +33,8 @@ function storeUser (request, response) {
   client.query(SQL, values)
     .then( () => {
       response.render('pages/main/event.ejs');
-    });
+    })
+    .catch(() => errorHandler('Error 500 ! Something has gone!', request, response));
 }
 
 app.post('/guestList', (req, res) => {
@@ -43,6 +44,17 @@ app.post('/guestList', (req, res) => {
 app.post('/menuRender', (req, res) => {
   res.render('pages/main/menuRender.ejs');
 });
+
+function savedDatabase(req, res) {
+  let SQL = `SELECT * FROM drinks`;
+
+  client.query(SQL)
+    .then(data => {
+      console.log(data);
+      res.render('pages/main/menuRender.ejs', { databaseResults: data.rows, });
+    })
+    .catch(() => errorHandler('Error 500 ! Something has gone!', req, res));
+}
 
 function homePage(req, res) {
   res.render('pages/index.ejs');
@@ -85,6 +97,10 @@ function Drinks(info) {
 }
 
 app.get('*', (req, response) => response.status(404).send('This route does not exist'));
+
+function errorHandler(error, req, response) {
+  response.status(500).send(error);
+}
 
 // function startServer(){
 //   const PORT = process.env.PORT || 3000;
